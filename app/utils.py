8e31a5f551fa41855372744c9bbefb9bb0905d90
Jake Atwell <jake.atwell@gmail.com>
2025-07-11 21:46:27 -0400

    Implement user-specific vote counting logic

--- app/utils.py
+++ app/utils.py
@@ -1,7 +1,13 @@
-from django.db.models import Count
+from django.db.models import Count, Q
 
 from .models import Quote
 
 
 def get_quotes_with_top_n_votes(n=None) -> list[Quote]:
     return Quote.objects.annotate(win_count=Count("winner")).order_by("-win_count")[:n]
+
+
+def get_quotes_with_top_n_votes_for_user(user_id, n=None) -> list[Quote]:
+    return Quote.objects.annotate(
+        win_count=Count("winner", filter=Q(winner__user_id=user_id))
+    ).order_by("-win_count")[:n]
